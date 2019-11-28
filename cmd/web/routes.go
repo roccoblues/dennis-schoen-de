9673e5f4b88edb6e82e0d3583e92b100e2d2e797
Dennis Schön <mail@dennis-schoen.de>
2019-11-28 16:51:38 +0100

    HandlerFunc over Handle

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,11 +8,15 @@ import (
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/resume", app.resume)
+	mux.HandleFunc("/", app.defaultMiddleware(app.home))
+	mux.HandleFunc("/resume", app.defaultMiddleware(app.resume))
 
 	fileServer := http.FileServer(pkger.Dir("/ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return mux
+}
+
+func (app *application) defaultMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return app.recoverPanic(app.logRequest(secureHeaders(next)))
 }
