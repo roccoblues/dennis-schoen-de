5433511431b6cf6963e11767f9662d7c722ace2b
Dennis Schön <mail@dennis-schoen.de>
2019-12-02 21:51:18 +0100

    fix stripping of port from request host

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net"
 	"net/http"
+	"strings"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -29,14 +30,21 @@ func (app *application) resume(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) httpsRedirect(tlsPort string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		host, _, err := net.SplitHostPort(r.Host)
-		if err != nil {
-			app.serverError(w, err)
-			return
-		}
 		u := r.URL
-		u.Host = net.JoinHostPort(host, tlsPort)
+		u.Host = net.JoinHostPort(stripPort(r.Host), tlsPort)
 		u.Scheme = "https"
 		http.Redirect(w, r, u.String(), http.StatusMovedPermanently)
 	}
+}
+
+// https://github.com/golang/go/commit/1ff19201fd898c3e1a0ed5d3458c81c1f062570b#diff-6c2d018290e298803c0c9419d8739885R971-R980
+func stripPort(hostport string) string {
+	colon := strings.IndexByte(hostport, ':')
+	if colon == -1 {
+		return hostport
+	}
+	if i := strings.IndexByte(hostport, ']'); i != -1 {
+		return strings.TrimPrefix(hostport[:i], "[")
+	}
+	return hostport[:colon]
 }
